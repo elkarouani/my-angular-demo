59e649479c5a80586598e53780b3aeaeba8c8a1f
KDragon <elkarouani@gmail.com>
2020-01-22 23:38:30 +0100

    Task: playing with all form types on contact form html part

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'; @Component({ selector:
 'contact-form', templateUrl: './contact-form.component.html', styleUrls:
 ['./contact-form.component.sass'] }) export class ContactFormComponent
-implements OnInit { constructor() { } ngOnInit() { } log(firstName) {
-console.log(firstName); } submit(f) { console.log(f); } }
+implements OnInit { ContactMethods = [{ id: 1, name: 'email' }, { id: 2, name:
+'telephone' }, {id: 3, name: 'sms'}]; constructor() { } ngOnInit() { }
+log(firstName) { console.log(firstName); } submit(f) { console.log(f); } }
