5169e250a9ae93c352ac6fc3bc344dffa4d2bdaf
KDragon <elkarouani@gmail.com>
2020-01-23 00:31:12 +0100

    Task: discovering form group and form control of angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,31 @@
-import { CoursesService } from './courses.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CoursesService } from "./courses.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CourseComponent } from './course/course.component';
-import { ResumePipe } from './resume.pipe';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { PanelComponent } from './panel/panel.component';
-import { InputFormatDirective } from './input-format.directive';
-import { ContactFormComponent } from './contact-form/contact-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CourseComponent } from "./course/course.component";
+import { ResumePipe } from "./resume.pipe";
+import { FavoriteComponent } from "./favorite/favorite.component";
+import { PanelComponent } from "./panel/panel.component";
+import { InputFormatDirective } from "./input-format.directive";
+import { ContactFormComponent } from "./contact-form/contact-form.component";
+import { SignupFormComponent } from "./signup-form/signup-form.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CourseComponent,
-    ResumePipe,
-    FavoriteComponent,
-    PanelComponent,
-    InputFormatDirective,
-    ContactFormComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [CoursesService],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		CourseComponent,
+		ResumePipe,
+		FavoriteComponent,
+		PanelComponent,
+		InputFormatDirective,
+		ContactFormComponent,
+		SignupFormComponent
+	],
+	imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+	providers: [CoursesService],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,6 +1,21 @@
-import { Component, OnInit } from '@angular/core'; @Component({ selector:
-'contact-form', templateUrl: './contact-form.component.html', styleUrls:
-['./contact-form.component.sass'] }) export class ContactFormComponent
-implements OnInit { ContactMethods = [{ id: 1, name: 'email' }, { id: 2, name:
-'telephone' }, {id: 3, name: 'sms'}]; constructor() { } ngOnInit() { }
-log(firstName) { console.log(firstName); } submit(f) { console.log(f); } }
+import { Component, OnInit } from "@angular/core";
+@Component({
+	selector: "contact-form",
+	templateUrl: "./contact-form.component.html",
+	styleUrls: ["./contact-form.component.sass"]
+})
+export class ContactFormComponent implements OnInit {
+	ContactMethods = [
+		{ id: 1, name: "email" },
+		{ id: 2, name: "telephone" },
+		{ id: 3, name: "sms" }
+	];
+	constructor() {}
+	ngOnInit() {}
+	log(firstName) {
+		console.log(firstName);
+	}
+	submit(f) {
+		console.log(f);
+	}
+}
